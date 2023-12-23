c4ec59ebc3e7a8379fc31da8bd440a9254716043
Mitch <mitchell.burcheri@gmail.com>
2023-12-24 01:48:40 +1100

    Create scan-ports.sh
    Uploaded TCP port scan script using NMAP

---
+++ scan-ports.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [[ -z "$1" || -z "$2" ]]; then
+    echo "Usage: scan-ports.sh <ip> <output>"
+    exit 0
+fi
+
+echo "Discovering TCP ports..."
+nmap -T4 -p- "$1" -oN "$2"
+ports=$(cat "$2" | grep / | grep open | awk -F/ '{print $1}' | tr '\n' ',')
+ports=${ports::-1}
+echo
+echo
+echo "Running full scan on discovered TCP ports $ports... "
+echo
+echo
+nmap -T4 -p$ports "$1" -oN "$2"
+echo
+echo
+echo "Scans complete. Results saved to: $2"
